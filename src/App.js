a151fe24e2d58e01269e8e63dd219a08ca15d603
Nico Prananta <nicnocquee@users.noreply.github.com>
2020-03-27 10:15:16 +0100

    add pay button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [showPayment, setShowPayment] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="https://swift.payrexx.com/pay?tid=b3aedd54" target="_blank">
+        Pay in New Window
+      </a>
+      <br />
+      <br />
+      <button className="pay-button" onClick={() => setShowPayment(true)}>
+        Pay
+      </button>
+      {showPayment && (
+        <div className="payment-modal" onClick={() => setShowPayment(false)}>
+          <iframe
+            className="payment-iframe"
+            src="https://swift.payrexx.com/pay?tid=b3aedd54&appview=1"
+            width="50%"
+            height="800"
+            id="payrexx-embed"
+          ></iframe>
+        </div>
+      )}
     </div>
   );
 }
